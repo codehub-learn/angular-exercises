15aab7df2b38c1d3e50cde63cf6b46b7eeaa5f10
Aristeidis Bampakos <bampakoa@gmail.com>
2022-04-11 19:48:07 +0300

    Add consuming parameters routing demo

--- ng-playground/src/app/dashboard/dashboard-routing.module.ts
+++ ng-playground/src/app/dashboard/dashboard-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ConsumeParamsComponent } from './consume-params/consume-params.component';
 import { ParentAComponent } from './parent-a/parent-a.component';
 import { ParentBComponent } from './parent-b/parent-b.component';
 
 const routes: Routes = [
   { path: 'parenta', component: ParentAComponent },
-  { path: 'parentb', component: ParentBComponent }
+  { path: 'parentb', component: ParentBComponent },
+  { path: 'consume-params/:id', component: ConsumeParamsComponent }
 ];
 
 @NgModule({
--- ng-playground/src/app/dashboard/dashboard.module.ts
+++ ng-playground/src/app/dashboard/dashboard.module.ts
@@ -5,13 +5,15 @@ import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
 import { ParentAComponent } from './parent-a/parent-a.component';
 import { ParentBComponent } from './parent-b/parent-b.component';
+import { ConsumeParamsComponent } from './consume-params/consume-params.component';
 
 
 @NgModule({
   declarations: [
     DashboardComponent,
     ParentAComponent,
-    ParentBComponent
+    ParentBComponent,
+    ConsumeParamsComponent
   ],
   imports: [
     CommonModule,
